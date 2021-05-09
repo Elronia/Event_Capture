1d57d9ad3b255171e6f7343460f7ab5b816436a5
Elronia <zar.catherine@gmail.com>
2021-05-09 15:02:52 -0400

    stopping bubbling

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
     console.log(this.classList.value);
-    console.log(this);
+    e.stopPropagation(); //stop bubbling
+    // console.log(this);
 }
 
 document.body.addEventListener('click', logText);
 
-divs.forEach(div => div.addEventListener('click', logText));
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false
+}));
