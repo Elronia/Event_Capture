4ed614149b33903ae1a2e3bec9a35218f7e04ee2
Elronia <zar.catherine@gmail.com>
2021-05-09 22:01:52 -0400

    adding EventListener for the button, unbinding so that the button can be clicked only once

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 function logText(e) {
     console.log(this.classList.value);
-    e.stopPropagation(); //stop bubbling
+    // e.stopPropagation(); //stop bubbling
     // console.log(this);
 }
 
 document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText, {
-    capture: false
+    capture: false,
+    once: true
 }));
+
+button.addEventListener('click', () => {
+    console.log('Click!!!');
+}, {
+    once: true
+})
